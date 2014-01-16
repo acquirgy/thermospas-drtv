cbbf62c071949a4a52e4a97d7700ad4ac70c1ff9
Roeland Reyniers <roeland@bythepixel.com>
2014-01-16 15:32:46 -0700

    Update base_url.

--- application/config/config_staging.php
+++ application/config/config_staging.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url'] = 'thermospa.dnadv.net';
+$config['base_url'] = 'http://thermospa.dnadv.net';
 
 /*
 |--------------------------------------------------------------------------
