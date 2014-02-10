9d22a6c776f08f423a48bd51488305c0c7be80ac
Cindy <crubin@acquirgy.com>
2014-02-10 11:15:01 -0500

    add leads reporting with date filter

--- application/controllers/admin/leads.php
+++ application/controllers/admin/leads.php
@@ -9,7 +9,20 @@ class Leads extends MY_Controller {
   public function index() {
     $data['leads'] = $this->lead_model->get_all();
 
-   $this->load->view('admin/leads', $data);
+    $this->load->view('admin/leads', $data);
+  }
+
+  public function filtered() {
+    $start = $this->input->post('dateStart');
+    $end = $this->input->post('dateEnd');
+
+    if ($start > 0 && $end > 0) {
+      $data['leads'] = $this->lead_model->get_many_by(array('date >=' => $start, 'date <' => $end));
+    } else {
+      $data['leads'] = $this->lead_model->get_all();
+    }
+
+    $this->load->view('admin/leads', $data);
   }
 
 }
--- application/views/admin/leads.php
+++ application/views/admin/leads.php
@@ -5,8 +5,17 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin - Thermospas DRTV Leads</title>
+
   <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.3.0/pure-min.css">
   <link rel="stylesheet" href="/assets/stylesheets/shared/baby-blue.css">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+  <script>
+  $(function() {
+    $( ".datepicker" ).datepicker({ dateFormat: 'yy-mm-dd' });
+  });
+  </script>
 </head>
 <body>
  <div class="pure-g-r" id="layout">
@@ -23,7 +32,25 @@
     <div class="header">
       <h1>Leads for Thermospas DRTV</h1>
     </div>
+    <?= form_open('/admin/leads/filtered', array('class' => 'filters-form pure-form')) ?>
+    <div class="pure-g">
 
+      <div class="pure-u-1-4">
+        <h4>Set Date Range of Leads:</h4>
+        <fieldset class="pure-group">
+          <input name="dateStart" type="text" id="dateStart" class="datepicker" value="" placeholder="start"  />
+          <input name="dateEnd" type="text" id="dateEnd" class="datepicker" value="" placeholder="end" />
+        </fieldset>
+      </div>
+
+    </div>
+
+    <div class="pure-controls">
+      <button type="submit" class="get-filtered-button pure-button pure-button-primary get_filtered">Get Leads</button>
+    </div>
+    <hr />
+
+  <?= form_close() ?>
     <table class="pure-table pure-table-bordered">
       <thead>
         <tr>
