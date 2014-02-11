78035b634f2f0e0f1212b318f1ea5107731615de
Roeland Reyniers <roeland@bythepixel.com>
2014-02-11 13:40:33 -0700

    Update base url for staging and production.

--- application/config/config_staging.php
+++ application/config/config_staging.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url'] = 'http://thermospa.dnadv.net';
+$config['base_url'] = 'http://thermospas.dnadv.net';
 
 /*
 |--------------------------------------------------------------------------
