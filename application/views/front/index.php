692361ce5383ada454ca1299a370a1968d094233
Cindy <crubin@acquirgy.com>
2014-03-27 13:58:09 -0400

    reorder states in dropdown

--- application/views/front/index.php
+++ application/views/front/index.php
@@ -60,56 +60,57 @@
 										<input class="text3" type="text" placeholder="City" name="city">
 										<select class="select" name="state">
 											<option>State</option>
-											<option>ID</option>
-											<option>IA</option>
-											<option>AL</option>
-											<option>AK</option>
-											<option>AZ</option>
-											<option>AR</option>
-											<option>WY</option>
-											<option>WA</option>
-											<option>VT</option>
-											<option>VA</option>
-											<option>WI</option>
-											<option>HI</option>
-											<option>DE</option>
-											<option>GA</option>
-											<option>WV</option>
-											<option>IL</option>
-											<option>IN</option>
-											<option>CA</option>
-											<option>KS</option>
-											<option>KY</option>
-											<option>CO</option>
-											<option>CT</option>
-											<option>LA</option>
-											<option>MA</option>
-											<option>MN</option>
-											<option>MS</option>
-											<option>MO</option>
-											<option>MI</option>
-											<option>MT</option>
-											<option>ME</option>
-											<option>MD</option>
-											<option>NE</option>
-											<option>NV</option>
-											<option>NH</option>
-											<option>NJ</option>
-											<option>NY</option>
-											<option>NM</option>
-											<option>OH</option>
-											<option>OK</option>
-											<option>OR</option>
-											<option>PA</option>
-											<option>RI</option>
-											<option>ND</option>
-											<option>NC</option>
-											<option>TN</option>
-											<option>TX</option>
-											<option>FL</option>
-											<option>SD</option>
-											<option>SC</option>
-											<option>UT</option>
+											<option value="AL">AL</option>
+                      <option value="AK">AK</option>
+                      <option value="AZ">AZ</option>
+                      <option value="AR">AR</option>
+                      <option value="CA">CA</option>
+                      <option value="CO">CO</option>
+                      <option value="CT">CT</option>
+                      <option value="DE">DE</option>
+                      <option value="DC">DC</option>
+                      <option value="FL">FL</option>
+                      <option value="GA">GA</option>
+                      <option value="HI">HI</option>
+                      <option value="ID">ID</option>
+                      <option value="IL">IL</option>
+                      <option value="IN">IN</option>
+                      <option value="IA">IA</option>
+                      <option value="KS">KS</option>
+                      <option value="KY">KY</option>
+                      <option value="LA">LA</option>
+                      <option value="ME">ME</option>
+                      <option value="MD">MD</option>
+                      <option value="MA">MA</option>
+                      <option value="MI">MI</option>
+                      <option value="MN">MN</option>
+                      <option value="MS">MS</option>
+                      <option value="MO">MO</option>
+                      <option value="MT">MT</option>
+                      <option value="NE">NE</option>
+                      <option value="NV">NV</option>
+                      <option value="NH">NH</option>
+                      <option value="NJ">NJ</option>
+                      <option value="NM">NM</option>
+                      <option value="NY">NY</option>
+                      <option value="NC">NC</option>
+                      <option value="ND">ND</option>
+                      <option value="OH">OH</option>
+                      <option value="OK">OK</option>
+                      <option value="OR">OR</option>
+                      <option value="PA">PA</option>
+                      <option value="RI">RI</option>
+                      <option value="SC">SC</option>
+                      <option value="SD">SD</option>
+                      <option value="TN">TN</option>
+                      <option value="TX">TX</option>
+                      <option value="UT">UT</option>
+                      <option value="VT">VT</option>
+                      <option value="VA">VA</option>
+                      <option value="WA">WA</option>
+                      <option value="WV">WV</option>
+                      <option value="WI">WI</option>
+                      <option value="WY">WY</option>
 										</select>
 										<div class="row-area">
 											<input id="Zip" class="text4 required-zip" type="text" placeholder="Zip*" name="zip">
