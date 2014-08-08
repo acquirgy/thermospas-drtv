3f27b70fd95b79b21b4305532322f75eb4820823
Roeland Reyniers <roeland@bythepixel.com>
2014-08-08 11:47:16 -0600

    Add sharpspring tracking to main form.

--- application/views/front/index.php
+++ application/views/front/index.php
@@ -77,6 +77,12 @@
                 </div>
               </fieldset>
               <?= form_close() ?>
+		<script type="text/javascript">
+		    var __ss_noform = __ss_noform || [];
+		    __ss_noform.push(['baseURI', 'https://app-PLBR48.sharpspring.com/webforms/receivePostback/MzQyNQAA/']);
+		    __ss_noform.push(['endpoint', '77882009-d797-4a89-a905-1a5e2f7b12f7']);
+		</script>
+		<script type="text/javascript" src="https://koi-PLBR48.sharpspring.com/client/noform.js?ver=1.0" ></script>              
 						</div>
 					</div>
 				</div>
